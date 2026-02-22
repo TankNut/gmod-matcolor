8c5edc1bbec8e0e577bfd9f3ff2e802f048ad66c
TankNut <m.vlasblom@live.nl>
2026-02-22 21:08:15 +0100

    More workshop stuff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# matcolor
+# Material color tool
+A tool for easily coloring individual materials on a model.
 
+### [Workshop link](https://steamcommunity.com/sharedfiles/filedetails/?id=3671618206)
--- workshop.bat
+++ workshop.bat
@@ -1,8 +1,10 @@
+set WORKSHOP_ID=3671618206
+
 cd /d "%~dp0"
 cd ..\..\..\bin\
 
 gmad create -folder "%~dp0\" -out "%~dp0..\__TEMP.gma"
-gmpublish create -addon "%~dp0..\__TEMP.gma" -icon "%~dp0icon.jpg"
+gmpublish update -addon "%~dp0..\__TEMP.gma" -id "%WORKSHOP_ID%"
 
 cd /d "%~dp0"
 
